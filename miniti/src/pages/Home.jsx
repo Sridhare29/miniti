85615e210d1c80eaa878f37e90ebcf81a0656ed6
Sridhar <sridhar.elumalai@kanini.com>
2024-11-09 14:28:44 +0530

    react-dom setup

---
+++ miniti/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+
+function Home() {
+  return (
+    <div className='w-full'>
+        <Header/>
+      <h2>Home!</h2>
+    </div>
+  )
+}
+
+export default Home
