7576d932e88460f0a37a63f5f183acaf2f96df6f
Sridhar <sridhar.elumalai@kanini.com>
2024-11-10 22:38:30 +0530

    add new file & route

--- miniti/src/App.jsx
+++ miniti/src/App.jsx
@@ -1,10 +1,24 @@
 import  { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
+import Category from './pages/Category';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <BrowserRouter>
+    <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/mens' element={<Category category="men"/>}/>
+      <Route path='/womens' element={<Category category="women"/>}/>
+      <Route path='/kids' element={<Category category="kid"/>}/>
+      <Route path='/product' element={<Product/>}>
+        <Route path=':productId' element={<Product/>}/>
+      </Route>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/login' element={<Login/>}/>
     </Routes>
     </BrowserRouter>
   );
--- miniti/src/pages/Home.jsx
+++ miniti/src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import Header from '../components/Navbar'
 
 function Home() {
   return (
     <div className='w-full'>
-        <Header/>
       <h2>Home!</h2>
     </div>
   )
