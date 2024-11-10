961d347f8eb21497b69b888ce904b20ad35c02da
Sridhar <sridhar.elumalai@kanini.com>
2024-11-10 20:22:17 +0530

    navbar & img

---
+++ miniti/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../assets/logo.png'
+import cart_icon from '../assets/cart_icon.png'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='nav-logo'>
+            <img src={logo} alt=''/>
+            <p>Miniti</p>
+        </div>    
+        <ul className="nav-menu">
+          <li>Shop <hr/></li>
+          <li>Men</li>
+          <li>Women</li>
+          <li>Kids</li>
+        </ul> 
+        <div className="nav-cart">
+            <button>Login</button>
+            <img src={cart_icon} alt=''/>
+            <div className='nav-cart-count'>0</div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
