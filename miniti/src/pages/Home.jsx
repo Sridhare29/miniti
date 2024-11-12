5eb6d1c553367f514ff4fe9332b0a1f9899eb8f0
Sridhar <sridhar.elumalai@kanini.com>
2024-11-12 21:37:53 +0530

    popular sec

--- miniti/src/pages/Home.jsx
+++ miniti/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
+import Popular from '../components/Popular/Popular'
 
 function Home() {
   return (
     <div className='w-full'>
         <Hero/>
       <h2>Home!</h2>
+      <Popular/>
     </div>
   )
 }
