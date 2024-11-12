21f51dde9b35250d9f68bfb430eb581fd54e65dc
Sridhar <sridhar.elumalai@kanini.com>
2024-11-12 22:40:33 +0530

    banner in home

--- miniti/src/pages/Home.jsx
+++ miniti/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
 import Popular from '../components/Popular/Popular'
+import OfferBanner from '../components/Banner/OfferBanner'
 
 function Home() {
   return (
     <div className='w-full'>
-        <Hero/>
-      <h2>Home!</h2>
+      <Hero/>
       <Popular/>
+      <OfferBanner/>
     </div>
   )
 }
