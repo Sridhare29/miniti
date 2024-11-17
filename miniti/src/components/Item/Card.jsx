62ebffbfd2f70185314ab9d3fa603d2ed006e417
Sridhar <sridhar.elumalai@kanini.com>
2024-11-17 22:48:29 +0530

    bread crum navigate & setup

--- miniti/src/components/Item/Card.jsx
+++ miniti/src/components/Item/Card.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Card.css'
+import { Link } from 'react-router-dom'
 
 function Card (props) {
   return (
     <div className='item'>
-      <img src={props.image} alt=''/>
+      <Link to={`/product/${props.id}`}><img src={props.image} alt=''/></Link>
       <p>{props.name}</p>
       <div className="item-prices">
         <div className="item-price-new">
--- miniti/src/pages/Product.jsx
+++ miniti/src/pages/Product.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import ShopContext from '../context/ShopContext'
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../components/Breadcrum/breadcrum';
 
 function Product() {
+  const {all_product} = useContext(ShopContext);
+  const {product_Id} = useParams();
+  const product = all_product.find((e)=> e.id === Number(product_Id));
+
   return (
     <div>
-      
+      <Breadcrum product={product}/>
     </div>
   )
 }
