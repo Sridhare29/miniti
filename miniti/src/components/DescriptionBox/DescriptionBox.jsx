81fadf15825c3c9c0180ec8ef7f0da5bf845b72a
Sridhar <sridhar.elumalai@kanini.com>
2024-11-21 20:39:17 +0530

    add DescriptionBox & RelatedProducts

---
+++ miniti/src/components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-Navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Review (122)</div>
+
+        </div>
+      <div className="descriptionbox-description">
+        <p>
+        Clothing refers to garments made from various materials such as fabric, leather, or synthetic fibers, designed to cover and protect the body. They serve functional purposes like warmth and durability while also reflecting personal style, cultural traditions, and fashion trends. Clothing ranges from casual wear to formal attire, catering to different occasions and climates.
+        </p>
+        <p>
+        Clothing refers to garments made from various materials such as fabric, leather, or synthetic fibers, designed to cover and protect the body. They serve functional purposes like warmth and durability while also reflecting personal style, cultural traditions, and fashion trends. Clothing ranges from casual wear to formal attire, catering to different occasions and climates.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
--- miniti/src/pages/Product.jsx
+++ miniti/src/pages/Product.jsx
@@ -3,6 +3,8 @@ import ShopContext from '../context/ShopContext'
 import { useParams } from 'react-router-dom';
 import Breadcrum from '../components/Breadcrum/breadcrum';
 import ProductView from '../components/ProductView/ProductView';
+import DescriptionBox from '../components/DescriptionBox/DescriptionBox';
+import RelatedProducts from '../components/RelatedProducts/RelatedProducts';
 
 function Product() {
   const { all_product } = useContext(ShopContext);
@@ -13,6 +15,8 @@ function Product() {
     <div>
       <Breadcrum product={productdata}/>
       <ProductView product={productdata}/>
+      <DescriptionBox />
+      <RelatedProducts/>
     </div>
   )
 }
