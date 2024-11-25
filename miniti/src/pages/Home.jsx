8dfcc27e50447434060d6b32c177500ccd12123d
Sridhar <sridhar.elumalai@kanini.com>
2024-11-25 22:18:46 +0530

    Qoute Banner

--- miniti/src/pages/Home.jsx
+++ miniti/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import MainBanner from '../components/Layout/MainBanner'
+import { QuoteBanner } from '../components/Layout/QuoteBanner'
 
 function Home() {
   return (
     <div className='w-full'>
         <Header/>
         <MainBanner/>
+        <QuoteBanner/>
     </div>
   )
 }
