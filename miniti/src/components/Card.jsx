b3115f3223f399f6eb4b3c3a79f57e4f207b256e
Sridhar <sridhar.elumalai@kanini.com>
2024-11-26 13:34:41 +0530

    my change

--- miniti/src/components/Card.jsx
+++ miniti/src/components/Card.jsx
@@ -2,17 +2,20 @@ import React from "react";
 
 function Card(props) {
   return (
-    <div className="w-fit flex m-4 bg-green-200">
-      <div className=" flex  bg-orange-200">
+    <div className="flex flex-col lg:flex-row items-center w-full max-w-sm lg:max-w-md bg-green-100 rounded-lg shadow-lg overflow-hidden m-4">
+      {/* Image Section */}
+      <div className="flex-shrink-0  p-2">
         <img
           src={props.image}
           alt="Recycling Illustration"
-          className="mt-4 w-40 h-50 p-2 items-center lg:w-36"
+          className="w-full object-contain"
         />
       </div>
-      <div className=" flex flex-col w-fit bg-blue-300">
-        <p className="p-2 text-lg font-semibold">{props.titile}</p>
-        <p className="p-2 text-sm">{props.decription}</p>
+
+      {/* Text Content Section */}
+      <div className="flex flex-col justify-center bg-blue-100 p-4 w-full">
+        <h3 className="text-lg font-bold text-gray-800">{props.title}</h3>
+        <p className="mt-2 text-sm text-gray-600">{props.description}</p>
       </div>
     </div>
   );
--- miniti/src/components/Layout/AboutBanner.jsx
+++ miniti/src/components/Layout/AboutBanner.jsx
@@ -2,10 +2,27 @@ import React from 'react'
 import Card from '../Card'
 
 const AboutBanner = () => {
+    const cardValue = {
+        id : 1,
+        title : 'Best Price',
+        description : "descriptiondescriptiondescriptiondescriptiondescription description",
+        image: "http://localhost:3000/images/Garbage management-bro.svg",
+
+        id : 2,
+        title : 'Best Price',
+        description : "descriptiondescriptiondescriptiondescriptiondescription description",
+        image: "http://localhost:3000/images/Garbage management-bro.svg",
+
+        id : 3,
+        title : 'Best Price',
+        description : "descriptiondescriptiondescriptiondescriptiondescription description",
+        image: "http://localhost:3000/images/Garbage management-bro.svg",
+    };
+
   return (
     <div className='w-full mt-12'>
        <div className="flex justify-around">
-         <Card/>
+         <Card title={cardValue.title} description={cardValue.description} image={cardValue.image}/>
        </div>
     </div>
   )
