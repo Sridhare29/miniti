0d0279d51eab7a8691cb16d24b216e83cfe348db
Sridhar <sridhar.elumalai@kanini.com>
2025-01-17 00:11:05 +0530

    header ui

--- miniti/src/components/Header.jsx
+++ miniti/src/components/Header.jsx
@@ -25,166 +25,113 @@ const Header = () => {
   return (
     <div className="w-full bg-white">
       {/* Top Header Section */}
-      <div className="header-top bg-[#c1E6BA] md-lg:hidden">
+      <div className="header-top bg-[#c1E6BA] h-12 md-lg:hidden">
         <div className="container mx-auto px-4">
           <div className="flex justify-between items-center h-[50px] text-slate-500">
             {/* Contact Information */}
             <ul className="flex items-center gap-8 font-medium text-black">
               <li className="flex items-center gap-2 text-sm relative after:content-[''] after:absolute after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:right-[-16px]">
-                <Mail />
-                <span>green-support@miniti.com</span>
+                <Mail style={{ width: "16px", height: "16px" }}/>
+                <span className="text-xs">green-support@miniti.com</span>
               </li>
               <li className="flex items-center gap-2 text-sm">
-                <Phone />
-                <span>+(123) ECO-HELP</span>
+                <Phone style={{ width: "16px", height: "16px" }}/>
+                <span className="text-xs">+(91) ECO-HELP</span>
               </li>
             </ul>
+          </div>
+        </div>
+      </div>
+{/* Logo */}
+<div className="bg-white">
+  <div className="w-full lg:w-11/12 mx-auto">
+    <div className="h-[60px] flex justify-between items-center">
+      {/* Logo Section */}
+      <div className="w-1/4">
+        <Link to="/">
+          <img
+            className="h-[50px]"
+            src="http://localhost:3000/images/logo.png"
+            alt="Logo"
+          />
+        </Link>
+      </div>
 
-            {/* Social Media and User Options */}
-            <div className="flex items-center gap-8">
-              {/* Social Media Icons */}
-              <div className="flex items-center gap-4 text-black">
-                <a href="#" className="hover:text-blue-600 transition-colors">
-                  <Facebook />
-                </a>
-                <a href="#" className="hover:text-sky-500 transition-colors">
-                  <Twitter />
-                </a>
-                <a href="#" className="hover:text-blue-700 transition-colors">
-                  <Linkedin />
-                </a>
-                <a href="#" className="hover:text-gray-700 transition-colors">
-                  <Github />
-                </a>
-              </div>
+      {/* Navigation */}
+      <div className="w-3/4 flex items-center justify-between">
+        <ul className="flex items-center gap-6 text-sm font-medium p-2 border-2 border-[#ededed] bg-[#fffff] rounded-2xl">
+          <li>
+            <Link
+              to="/"
+              className={`p-1 ${
+                pathname === "/" ? "text-[#4DA674]" : "text-slate-600"
+              }`}
+            >
+              Marketplace
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/shop"
+              className={`p-1 ${
+                pathname === "/shop" ? "text-[#4DA674]" : "text-slate-600"
+              }`}
+            >
+              Recycle Hub
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/blog"
+              className={`p-1 ${
+                pathname === "/blog" ? "text-[#4DA674]" : "text-slate-600"
+              }`}
+            >
+              Eco Insights
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className={`p-1 ${
+                pathname === "/about" ? "text-[#4DA674]" : "text-slate-600"
+              }`}
+            >
+              Our Mission
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/contact"
+              className={`p-1 ${
+                pathname === "/contact" ? "text-[#4DA674]" : "text-slate-600"
+              }`}
+            >
+              Connect
+            </Link>
+          </li>
+        </ul>
 
-              {/* User Login/Account */}
-              {user ? (
-                <div className="flex items-center gap-2 text-sm text-black hover:text-green-600 transition-colors cursor-pointer">
-                  <User />
-                  <span>Eco Warrior</span>
-                </div>
-              ) : (
-                <div className="flex items-center gap-2 text-sm text-black hover:text-green-600 transition-colors cursor-pointer">
-                  <Lock />
-                  <span>Join Green Movement</span>
-                </div>
-              )}
+        {/* Cart and Wishlist */}
+        <div className="flex items-center mr-6 gap-3">
+          <div className="relative flex items-center cursor-pointer p-2 rounded-full bg-[#EAF8E7]">
+            <Heart className="text-[#4DA674] text-sm"   style={{ width: "18px", height: "18px" }}  />
+            <div className="w-[12px] h-[12px] absolute bg-red-500 rounded-full text-white text-[8px] flex items-center justify-center -top-[2px] -right-[2px]">
+              {wishlist_count}
+            </div>
+          </div>
+          <div className="relative flex items-center cursor-pointer p-2 rounded-full bg-[#EAF8E7]">
+            <ShoppingCart className="text-[#4DA674]"   style={{ width: "18px", height: "18px" }}             />
+            <div className="w-[12px] h-[12px] absolute bg-red-500 rounded-full text-white text-[8px] flex items-center justify-center -top-[2px] -right-[2px]">
+              {wishlist_count}
             </div>
           </div>
         </div>
       </div>
-      {/* logo  */}
-      <div className="w-white pb-8">
-        <div className="w-[100%] lg:w-[90%] mx-auto">
-          <div className="h-[80px] md-lg:h-[100px] flex justify-between items-center flex-wrap ">
-            <div className="md-lg:w-full w-3/12  md-lg:pt-4">
-              <div className="flex justify-between ">
-                <Link to="/">
-                  <img
-                    className="flex h-[80px] ml-8"
-                    src="http://localhost:3000/images/logo.png"
-                    alt=""
-                  />
-                </Link>
-                <div
-                  className="justify-center items-center w-[30px] h-[30px] bg-white text-slate-600 border border-slate-600 rounded-sm cursor-pointer lg:hidden md-lg:flex xl:hidden hidden"
-                  onClick={() => setShowShidebar(false)}
-                >
-                  <span>
-                    {" "}
-                    <CiMenuFries />{" "}
-                  </span>
-                </div>
-              </div>
-            </div>
-            {/* navbar topic head [#023336]- dark*/}
-            <div className="md:lg:w-full w-9/12">
-              <div className="flex justify-between md-lg:justify-center items-center flex-wrap ">
-                <ul className="flex items-center justify-center gap-8 font-bold text-lg md-lg:hidden h-[100px]">
-                  <li>
-                    <Link
-                      to="/"
-                      className={`p-2 block ${
-                        pathname === "/" ? "text-[#4DA674]" : "text-slate-600"
-                      } `}
-                    >
-                      Marketplace
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/shop"
-                      className={`p-2 block ${
-                        pathname === "/shop"
-                          ? "text-[#4DA674]"
-                          : "text-slate-600"
-                      } `}
-                    >
-                      Recycle hub
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/blog"
-                      className={`p-2 block ${
-                        pathname === "/blog"
-                          ? "text-[#4DA674]"
-                          : "text-slate-600"
-                      } `}
-                    >
-                      Eco insights
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/about"
-                      className={`p-2 block ${
-                        pathname === "/about"
-                          ? "text-[#4DA674]"
-                          : "text-slate-600"
-                      } `}
-                    >
-                      Our mission
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/contact"
-                      className={`p-2 block ${
-                        pathname === "/contact"
-                          ? "text-[#4DA674]"
-                          : "text-slate-600"
-                      } `}
-                    >
-                      Connect
-                    </Link>
-                  </li>
-                </ul>
-                {/* Cart and wishlist */}
-                <div className="flex md-lg:hidden items-center justify-center gap-5 w-[10%] h-[50%]">
-                  <div className="relative flex justify-center items-center cursor-pointer px-4 py-4 rounded-full bg-[#EAF8E7]">
-                    <span className="text-xl text-[#4DA674]">
-                      <Heart />
-                    </span>
-                    <div className="w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px]">
-                      {wishlist_count}
-                    </div>
-                  </div>
-                  <div className="relative flex justify-center items-center cursor-pointer px-4 py-4 rounded-full bg-[#EAF8E7]">
-                    <span className="text-xl text-[#4DA674]">
-                      <ShoppingCart />
-                    </span>
-                    <div className="w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px]">
-                      {wishlist_count}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    </div>
+  </div>
+</div>
+
 
       {/* only at sm - menu  visible    */}
       <div className="hidden md-lg:block">
